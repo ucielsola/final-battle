c741ecd6a84357003132d4bcf47282c4ca77455e
Uciel Sola <hello@ucielsola.com>
2021-09-19 13:30:21 -0300

    add styling

--- src/components/login/LoginSubmitError.js
+++ src/components/login/LoginSubmitError.js
@@ -4,14 +4,17 @@ const LoginSubmitError = ({ stateHandler }) => {
 	// stateHandler trae el useState del parent
 	return (
 		<div className='container'>
-			ERROR
-			<button
-				className='btn'
-				onClick={() => {
-					stateHandler(false);
-				}}>
-				Try Again
-			</button>
+			<p className='display-6 text-center text-danger'>Error!</p>
+			<p>Please check your credentials and try again</p>
+			<div className='container d-flex justify-content-center'>
+				<button
+					className='btn btn-primary text-light'
+					onClick={() => {
+						stateHandler(false);
+					}}>
+					Try Again
+				</button>
+			</div>
 		</div>
 	);
 };
