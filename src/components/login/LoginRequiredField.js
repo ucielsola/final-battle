d60d770316a4d7647e6a056335715eaa1f58022e
Uciel Sola <hello@ucielsola.com>
2021-09-19 11:26:49 -0300

    name update

--- src/components/login/LoginRequiredField.js
+++ src/components/login/LoginRequiredField.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Field, ErrorMessage } from "formik";
 import InputError from "./LoginError";
-const LoginField = (props) => {
+const LoginRequiredField = (props) => {
 	const { name, label, id, placeholder, type, } = props;
 
 	return (
@@ -20,34 +20,4 @@ const LoginField = (props) => {
 	);
 };
 
-export default LoginField;
-
-// <div className='mb-3'>
-// 	<label htmlFor='email' className='form-label'>
-// 		Email
-// 	</label>
-// 	<Field
-// 		id='email'
-// 		name='email'
-// 		placeholder='challenge@alkemy.com'
-// 		type='email'
-// 		required
-// 		className='form-control'
-// 	/>
-// 	<ErrorMessage name='email' component={InputError} />
-// </div>;
-
-// <div className='mb-3'>
-// 	<label htmlFor='password' className='form-label'>
-// 		Password
-// 	</label>
-// 	<Field
-// 		id='password'
-// 		name='password'
-// 		placeholder='react'
-// 		type='password'
-// 		required
-// 		className='form-control'
-// 	/>
-// 	<ErrorMessage name='password' component={InputError} />
-// </div>;
+export default LoginRequiredField;
