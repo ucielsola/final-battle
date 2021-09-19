1fa97b81d7424fab6cb9c1580e8877a6f8716aa2
Uciel Sola <hello@ucielsola.com>
2021-09-19 13:38:37 -0300

    add styling

--- src/components/login/LoginSubmitOk.js
+++ src/components/login/LoginSubmitOk.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const LoginSubmitOk = () => {
-	return <div className='container'>Success</div>;
+	return (
+		<div className='container'>
+			<p className='display-6 text-center text-success'>Success!</p>
+			<p className='text-center'>You are being redirected to the site</p>
+			<div className='container d-flex justify-content-center'>
+				<button
+					className='btn btn-primary text-light'
+					> Continue
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default LoginSubmitOk;
