4a6cf909ba75fce1e6ba63b0fba784c44891ce1c
Uciel Sola <hello@ucielsola.com>
2021-09-19 11:27:00 -0300

    add axios

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -3,6 +3,7 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import LoginRequiredField from "./LoginRequiredField";
 import LoginButton from "./LoginButton";
+import axios from "axios";
 
 const LoginForm = () => {
 	const initialVals = {
@@ -17,10 +18,20 @@ const LoginForm = () => {
 		password: Yup.string().required("Password is required"),
 	});
 
-	const handleSubmit = (values) => {
-		console.log("hola", values);
+	// const handleSubmit = (values) => {
+	// 	console.log("hola", values);
+	// };
+
+	const handleSubmit = async (values, { resetForm }) => {
+		await axios
+			.post("http://challenge-react.alkemy.org/", values)
+			.then((res) => localStorage.setItem("token", res.data.token))
+			.catch((err) => console.log(err));
+
+		resetForm();
 	};
 
+	
 	return (
 		<div className='container shadow p-3 bg-body rounded position-absolute top-50 start-50 translate-middle col-xs-12 col-sm-10 col-md-8 col-lg-4'>
 			<h2 className='text-center'>~ Final Battle ~</h2>
