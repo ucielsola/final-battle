b215a636e792c7ccc0937cc3855ed8e2b3c7b8f8
Uciel Sola <hello@ucielsola.com>
2021-09-20 01:16:07 -0300

    add router and components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import PrivateRoute from "./components/PrivateRoute";
+import TheHeader from "./components/TheHeader";
+import TheFooter from "./components/TheFooter";
+import LoginPage from "./components/login/LoginPage";
+import HomePage from "./components/home/HomePage";
+import SearchPage from "./components/search/SearchPage";
+import HeroPage from "./components/hero/HeroPage";
 import "./assets/styles.scss";
-import LoginForm from "./components/login/LoginForm";
+
 function App() {
 	return (
-		<div className='App container-fluid vh-100 bg-dark'>
+		<div className='container-fluid overflow-hidden  vh-100 bg-dark'>
+			<TheHeader />
 			<Router>
-				<LoginForm />
+				<Switch>
+					<Route path='/login' component={LoginPage} />
+					<PrivateRoute exact path='/' component={HomePage} />
+					<PrivateRoute path='/search' component={SearchPage} />
+					<PrivateRoute path='/hero' component={HeroPage} />
+				</Switch>
 			</Router>
+			<TheFooter />
 		</div>
 	);
 }
