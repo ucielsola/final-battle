eceb7a814155970c3a32b167c6c81202ad983823
Uciel Sola <hello@ucielsola.com>
2021-09-23 02:08:47 -0300

    add new file paths and bootstrap classes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
-import TheHeader from "./components/TheHeader";
-import TheFooter from "./components/TheFooter";
-import LoginPage from "./components/login/LoginPage";
-import HomePage from "./components/home/HomePage";
-import SearchPage from "./components/search/SearchPage";
-import HeroPage from "./components/hero/HeroPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { PrivateRoute } from "./components/PrivateRoute";
+import { TheHeader } from "./components/TheHeader";
+import { TheFooter } from "./components/TheFooter";
+import { LoginPage } from "./pages/LoginPage";
+import { HomePage } from "./pages/HomePage";
+import { SearchPage } from "./pages/SearchPage";
+import { HeroPage } from "./pages/HeroPage";
 import "./assets/styles.scss";
 
 function App() {
 	return (
-		<div className='container-fluid overflow-hidden  vh-100 bg-dark'>
-			<TheHeader />
-			<Router>
+		<Router>
+			<div className='bg-dark vh-100'>
+				<TheHeader />
 				<Switch>
 					<Route path='/login' component={LoginPage} />
-					<PrivateRoute exact path='/' component={HomePage} />
+					<PrivateRoute path='/' exact component={HomePage} />
 					<PrivateRoute path='/search' component={SearchPage} />
 					<PrivateRoute path='/hero' component={HeroPage} />
 				</Switch>
-			</Router>
-			<TheFooter />
-		</div>
+				<TheFooter />
+			</div>
+		</Router>
 	);
 }
 
