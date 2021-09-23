a8d60115cd0512b9cf12d35f3f765886001eae07
Uciel Sola <hello@ucielsola.com>
2021-09-23 15:39:16 -0300

    add empty grid alert

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { HomeEmptyGrid } from "../components/home/HomeEmptyGrid";
 export const HomePage = () => {
+	const TeamGrid = false;
 	return (
 		<div className='animate__animated animate__fadeIn container pt-4 text-light'>
 			<div className='container-fluid'>
@@ -10,6 +11,7 @@ export const HomePage = () => {
 					<span className='text-primary'> Final Battle</span>?
 				</p>
 			</div>
+			{TeamGrid ? <div></div> : <HomeEmptyGrid />}
 		</div>
 	);
 };
