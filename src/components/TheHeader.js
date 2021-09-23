fbee669fb85e419491010f79a12bae5fcdd4d33d
Uciel Sola <hello@ucielsola.com>
2021-09-23 02:11:11 -0300

    add dummy nav

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const TheHeader = () => {
-  return (
-    <div>
-      HEADER
-    </div>
-  )
-}
-
-export default TheHeader
+export const TheHeader = () => {
+	return (
+		<div className='navbar navbar-expand-lg navbar-light border-bottom border-light'>
+			<Link to='/' className='text-decoration-none'>
+				<h1>Final Battle</h1>
+			</Link>
+			<nav>
+				<Link to='../'>Your Team</Link>
+				<Link to='../search'>Choose your Heros</Link>
+			</nav>
+		</div>
+	);
+};
