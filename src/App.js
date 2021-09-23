300ff5905702cdbc8155482a98db7364e042a8a2
Uciel Sola <hello@ucielsola.com>
2021-09-23 02:11:33 -0300

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { TheHeader } from "./components/TheHeader";
 import { TheFooter } from "./components/TheFooter";
