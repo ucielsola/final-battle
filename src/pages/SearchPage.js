c0bbce6712a9396814e12d62f1230ec6d4130b37
Uciel Sola <hello@ucielsola.com>
2021-09-25 00:47:08 -0300

    state for conditional rendering

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchInput } from "../components/search/SearchInput";
+import { SearchResults } from "../components/search/SearchResults";
 import "animate.css";
 
 export const SearchPage = () => {
+	const [results, setResults] = useState([]);
+
 	return (
 		<div className='animate__animated animate__fadeIn container pt-4 text-light'>
 			<div className='container-fluid'>
@@ -12,7 +15,14 @@ export const SearchPage = () => {
 					<span className='text-primary'> Final Battle</span>!
 				</p>
 			</div>
-			<SearchInput />
+			<SearchInput setResults={setResults} />
+			{results && (
+				<ul className='row row-cols-xl-3 mt-4 p-2 justify-content-center'>
+					{results.map((hero) => (
+						<SearchResults hero={hero} key={hero.id} />
+					))}
+				</ul>
+			)}
 		</div>
 	);
 };
