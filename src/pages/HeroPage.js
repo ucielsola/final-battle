20b2b4cf5ee0ddb819a3a237fcd2ebf1a2705a41
Uciel Sola <hello@ucielsola.com>
2021-09-25 01:15:47 -0300

    update with new components

--- src/pages/HeroPage.js
+++ src/pages/HeroPage.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
-import { Route, Redirect, useLocation } from "react-router";
-import { HeroStats } from "../components/hero/HeroStats";
-import { HeroInfo } from "../components/hero/HeroInfo";
+import React from "react";
+import { useLocation } from "react-router";
+import { HeroData } from "../components/hero/HeroData";
 
 export const HeroPage = () => {
 	const location = useLocation();
@@ -11,22 +10,26 @@ export const HeroPage = () => {
 	return (
 		<div className='animate__animated animate__fadeIn container pt-4 text-light'>
 			<div className='row'>
-				<div className='col-md-6 d-flex justify-content-center'>
-					<img alt={hero.name} src={hero.img} className='mx-auto' />
+				<div className='col-sm-12 col-md-6 d-flex justify-content-center'>
+					<img
+						alt={hero.name}
+						src={hero.img}
+						className='mw-100 mx-auto'
+					/>
 				</div>
-				<div className='col-sm-8 col-md-8 col-lg-6 px-4'>
+				<div className='col-sm-12 col-md-8 col-lg-6 py-4 px-4'>
 					<h2 className='display-4'>{hero.name}</h2>
 					<h3 className='display-6'>[ {hero.fullName} ]</h3>
 					<div className='row'>
 						<div className='col-6'>
-							<HeroStats hero={hero} />
+							<HeroData hero={hero} type='stats' />
 						</div>
 						<div className='col-6'>
-							<HeroInfo hero={hero} />
+							<HeroData hero={hero} type='info' />
 						</div>
 					</div>
 					<div className='row'>
-						<div className='col-md-6'>
+						<div className='col-4'>
 							<div className='row border p-2 text-center'>
 								<div className='col-md-4'>
 									<span className='display-6'>
@@ -42,17 +45,19 @@ export const HeroPage = () => {
 								</div>
 							</div>
 						</div>
-						<div className='col-md-3 d-flex align-items-center justify-content-center'>
+						<div className='col-4 d-flex align-items-center justify-content-center'>
 							<button
 								type='button'
-								className='btn btn-success btn-block'>
+								className='btn btn-success w-75'
+								title='Add to your Team'>
 								Hire
 							</button>
 						</div>
-						<div className='col-md-3 d-flex align-items-center justify-content-center'>
+						<div className='col-4 d-flex align-items-center justify-content-center'>
 							<button
 								type='button'
-								className='btn btn-block btn-danger'
+								className='btn btn-danger w-75'
+								title='Remove from your Team'
 								disabled>
 								Fire
 							</button>
