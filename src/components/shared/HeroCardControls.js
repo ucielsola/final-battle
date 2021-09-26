49989f9fc56c5216f03b7123a3048016cf5181ec
Uciel Sola <hello@ucielsola.com>
2021-09-26 20:20:58 -0300

    new components

---
+++ src/components/shared/HeroCardControls.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import { TeamContext } from "../../context/TeamState";
+
+export const HeroCardControls = ({ action, hero, exist, quota }) => {
+	const {
+		addToTeamGood,
+		addToTeamBad,
+		removeFromTeamGood,
+		removeFromTeamBad,
+	} = useContext(TeamContext);
+
+	const fireDisabled = exist && quota ? false : exist ? false : true;
+
+	return (
+		<>
+			{action === "hire" ? (
+				<button
+					className={`col-6 btn btn-success w-100 text-light text-capitalize`}
+					onClick={() => {
+						hero.alignment === "good"
+							? addToTeamGood(hero)
+							: addToTeamBad(hero);
+					}}
+					disabled={exist || quota}>
+					{action}
+				</button>
+			) : (
+				<button
+					className={`col-6 btn btn-danger w-100 text-light text-capitalize`}
+					onClick={() => {
+						hero.alignment === "good"
+							? removeFromTeamGood(hero.id)
+							: removeFromTeamBad(hero.id);
+					}}
+					disabled={fireDisabled}>
+					{action}
+				</button>
+			)}
+		</>
+	);
+};
