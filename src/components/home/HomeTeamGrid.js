1c64ce34c346e85408e09b1f8ec25344f722cbbb
Uciel Sola <hello@ucielsola.com>
2021-09-26 21:05:35 -0300

    add team grid for homepage

--- src/components/home/HomeTeamGrid.js
+++ src/components/home/HomeTeamGrid.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { TeamContext } from "../../context/TeamState";
+import { HomeHeroCard } from "../home/HomeHeroCard";
 
 export const HomeTeamGrid = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const { teamGood, teamBad } = useContext(TeamContext);
+
+	return (
+		<>
+			<div className='row'>
+				{teamGood.map((hero) => (
+					<li
+						className='search__results col-md-4 list-group-item bg-dark border-dark p-2 mw-80 animate__animated animate__fadeIn'
+						key={hero.id}>
+						<HomeHeroCard hero={hero} />
+					</li>
+				))}
+			</div>
+			<div className='row'>
+				{teamBad.map((hero) => (
+					<li
+						className='search__results col-md-4 list-group-item bg-dark border-dark p-2 mw-80 animate__animated animate__fadeIn'
+						key={hero.id}>
+						<HomeHeroCard hero={hero} />
+					</li>
+				))}
+			</div>
+		</>
+	);
+};
