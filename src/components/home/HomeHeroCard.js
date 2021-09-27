b17b68dd289ab4b7d931e4d1ab439b0207b3ae6d
Uciel Sola <hello@ucielsola.com>
2021-09-26 21:05:49 -0300

    add hero cards for homepage

---
+++ src/components/home/HomeHeroCard.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { HeroCardControls } from "../shared/HeroCardControls";
+import { TeamContext } from "../../context/TeamState";
+
+export const HomeHeroCard = ({ hero }) => {
+	const { teamGood, teamBad } = useContext(TeamContext);
+
+	return (
+		<div className='card p-2 vw-'>
+			<img
+				src={hero.img}
+				alt={hero.name}
+				className='card-img-top img-fluid mb-2 w-100'></img>
+			<div className='row'>
+				<h4 className='card-title col-6 text-dark'>
+					<strong>{hero.name}</strong>
+				</h4>
+				<h5 className='col-6 text-capitalize pt-1'>
+					{hero.alignment === "bad" ? "😈" : "😇"} {hero.alignment}
+				</h5>
+			</div>
+			<h6 className='card-subtitle text-dark'>{hero.fullName}</h6>
+			<Link
+				to={{
+					pathname: "/hero",
+					state: { hero: hero }, // pasa info del heroe a la página del heroe,
+				}}
+				className='btn btn-primary mt-2'>
+				Full Info
+			</Link>
+			<div className='mt-2'>
+				<HeroCardControls
+					hero={hero}
+					exist='true'
+					action='fire'
+					quota='false'
+				/>
+			</div>
+		</div>
+	);
+};
