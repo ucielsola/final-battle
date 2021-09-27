37e4ab7600963c250a19d7d4e1a3553e01b1cad6
Uciel Sola <hello@ucielsola.com>
2021-09-27 00:00:08 -0300

    add footer

---
+++ src/components/TheFooter.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const TheFooter = () => {
+	return (
+		<div className='badd-flex justify-content-center container border-top border-light bg-dark px-4'>
+			<span className='text-light'>
+				Made with <span> 💜 </span> by <span> </span>
+				<a
+					href='https://ucielsola.com'
+					title='Visit my portfolio!'
+					className='lead text-decoration-none'
+					target='_blank'
+					without
+					rel='noopener noreferrer'>
+					Uciel Sola
+				</a>
+				<span> || </span>
+				<span className='text-light'>
+					Final Batle <span> </span>
+					<a
+						href='https://github.com/ucielsola/final-battle'
+						title='Visit repo'
+						className='lead text-decoration-none'
+						target='_blank'
+						without
+						rel='noopener noreferrer'>
+						GitHub Repo
+					</a>
+				</span>
+			</span>
+		</div>
+	);
+};
