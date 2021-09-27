768638202a55efbf32aef2fa8722699bc504ef77
Uciel Sola <hello@ucielsola.com>
2021-09-27 20:32:22 -0300

    add id generator and import empty card

--- src/components/home/HomeTeamGrid.js
+++ src/components/home/HomeTeamGrid.js
@@ -1,30 +1,68 @@
 import React, { useContext } from "react";
 import { TeamContext } from "../../context/TeamState";
 import { HomeHeroCard } from "../home/HomeHeroCard";
+import { HomeEmptyCard } from "./HomeEmptyCard";
+import { HomeTeamStats } from "./HomeTeamStats";
 
 export const HomeTeamGrid = () => {
 	const { teamGood, teamBad } = useContext(TeamContext);
-
+	function guidGenerator() {
+		// random ID para cards en blanco
+		var S4 = function () {
+			return (((1 + Math.random()) * 0x10000) | 0)
+				.toString(16)
+				.substring(1);
+		};
+		return (
+			S4() +
+			S4() +
+			"-" +
+			S4() +
+			"-" +
+			S4() +
+			"-" +
+			S4() +
+			"-" +
+			S4() +
+			S4() +
+			S4()
+		);
+	}
 	return (
 		<>
-			<div className='row'>
+			<HomeTeamStats />
+			<ul className='row'>
 				{teamGood.map((hero) => (
 					<li
-						className='search__results col-md-4 list-group-item bg-dark border-dark p-2 mw-80 animate__animated animate__fadeIn'
+						className='search__results d-flex justify-content-center align-items-start col-md-4 list-group-item bg-dark border-dark p-2 mw-80 animate__animated animate__fadeIn'
 						key={hero.id}>
 						<HomeHeroCard hero={hero} />
 					</li>
 				))}
-			</div>
-			<div className='row'>
+				{teamGood.length > 0 && teamGood.length < 3 && (
+					<li
+						className='search__results d-flex justify-content-center align-items-start col-md-4 list-group-item bg-dark border-dark p-2 mw-80 animate__animated animate__fadeIn'
+						key={guidGenerator()}>
+						<HomeEmptyCard />
+					</li>
+				)}
+			</ul>
+			<ul className='row d-flex'>
 				{teamBad.map((hero) => (
 					<li
-						className='search__results col-md-4 list-group-item bg-dark border-dark p-2 mw-80 animate__animated animate__fadeIn'
+						className='search__results d-flex justify-content-center align-items-start col-md-4 list-group-item bg-dark border-dark p-2 mw-80 animate__animated animate__fadeIn'
 						key={hero.id}>
 						<HomeHeroCard hero={hero} />
 					</li>
 				))}
-			</div>
+				{teamBad.length > 0 && teamBad.length < 3 && (
+					<li
+						className='search__results d-flex justify-content-center align-items-start col-md-4 list-group-item bg-dark border-dark p-2 mw-80 animate__animated animate__fadeIn'
+						key={guidGenerator()}>
+						<HomeEmptyCard />
+					</li>
+				)}
+			</ul>
 		</>
 	);
 };
